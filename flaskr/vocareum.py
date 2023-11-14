592a7980c438df0ef1d6bbae91c82438d29107ab
dhsiehcode <89666559+dhsiehcode@users.noreply.github.com>
2023-11-13 22:20:16 -0500

    init

---
+++ flaskr/vocareum.py
@@ -0,0 +1,112 @@
+import requests
+import json
+
+'''
+
+author: Dennis Hsieh
+date: 11/13/2023
+
+
+
+'''
+
+'''
+
+author: Dennis Hsieh
+date: 11/13/2023
+
+gets all the assignments in a course given the id. returns 
+
+'''
+def get_assignments(auth_token, courseId):
+
+     url = f"https://api.vocareum.com/api/v2/courses/{courseId}/assignments"
+
+     headers = {"Authorization": f"Token {auth_token}"}
+
+     response = requests.get(url, headers=headers)
+
+     if response.status_code != 200:
+         print(f'Unable to get assignments: {response.status_code}')
+         return
+
+     response_dict = json.loads(response.json())
+
+     try:
+
+         assignments = {}  # store assignments with key: id and value: student
+
+
+
+
+     except KeyError:
+         print('issue reading assignments')
+         return
+
+
+'''
+
+gets all the students in a particular course
+
+auth_token: authentication token generated
+courseId: the id of the course which we want to get the students in
+
+'''
+def get_students(auth_token, courseId):
+
+    url = f"https://api.vocareum.com/api/v2/courses/{courseId}/users"
+
+    headers = {"Authorization":f"Token {auth_token}"}
+
+    #payload = {"role":""}
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code != 200:
+        print(f'Unable to get students: {response.status_code}')
+        return
+
+    response_dict = json.loads(response.json())
+
+    try:
+
+        students = {} # store courses with key: id and value: student
+
+        for student in response_dict['users']:
+            students[student['id']] = student['name']
+
+    except KeyError:
+        print('issue reading courses')
+        return
+
+
+'''
+gets all the current courses that exists with its id and name
+auth_token: authentication token generated
+
+'''
+def get_courses(auth_token):
+
+    url = "https://api.vocareum.com/api/v2/courses"
+
+    headers = {"Authorization": f"Token {auth_token}"}
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code != 200:
+        print(f'Unable to get courses: {response.status_code}')
+        return
+
+    response_dict = json.loads(response.json())
+
+    try:
+
+        courses = {} # store courses with key: id and value: course name
+
+        for course in response_dict['courses']:
+            courses[course['id']] = course['name']
+
+    except KeyError:
+        print('issue reading courses')
+        return
+
